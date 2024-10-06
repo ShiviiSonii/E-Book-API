457a61f19754e540f396ad949f86deea7b6efd3c
ShiviiSonii <1111dhruvsoni@gmail.com>
2024-10-06 15:53:31 +0530

    added global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import { globalErrorHandler } from "./middlewares/globalHandler";
 
 const app = express();
 
-//ROUTES
-app.get("/", (req, res) => {
+// ROUTES
+app.get("/", (_req, res) => {
   res.send("Welcome to E-book project");
 });
+
+// error-handling middleware function
+app.use(globalErrorHandler);
 
 export default app;
